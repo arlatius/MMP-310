bd92eefd75c74f25e563186d5142a0a93299892c
Arlatius <spencer.ayvas@gmail.com>
2020-09-14 19:10:42 -0400

    added project 2

---
+++ Project2/sketch.js
@@ -0,0 +1,192 @@
+/*
+project 1
+9/7/2020
+*/
+
+//global variables
+var kazuhiroIdleL, kazuhiroIdleR, kazuhiroWalkL, kazuhiroWalkR
+var kazuhiroX, kazuhiroY;
+var shadow;
+var flower, cloud;
+var kazuhiroSpeed = 7;
+var shadowY;
+
+function preload() {
+    kazuhiroIdleL = loadImage("../img/KazuhiroIdle32.gif");
+    kazuhiroIdleR = loadImage("../img/KazuhiroIdle32Right.gif");
+    kazuhiroWalkL = loadImage("../img/KazuhiroWalk32.gif");
+    kazuhiroWalkR = loadImage("../img/KazuhiroWalk32Right.gif");
+    shadow = loadImage("../img/Shadow32.png");
+    flower = loadImage("../img/Flower.png");
+    cloud = loadImage("../img/cloud.png");
+}
+
+function setup() {
+    createCanvas(940, 560);
+
+    imageMode(CENTER); //kazuhirox and kazuhiroy will now refer to the center of the image
+
+    kazuhiroX = width / 2;
+    kazuhiroY = height / 2;
+
+}
+
+function draw() {
+    background(200); //putting the bg here redraws it every frame so the frames of the transparent gif arent visible
+    
+//setting
+    //loop
+    
+//clouds
+    for (var cloudCounter = 0; cloudCounter < 3; cloudCounter += 1) {
+        image(cloud, cloudCounter * 150, 100);
+        image(cloud, cloudCounter * 300, 37);
+    }
+    
+    //var makes a variable that exists for the rest of the program, let creates a variable that only exists til the end of the associated curly bracket
+    //flowers
+    for(let x = 0; x < width; x += 100) {
+        image(flower, x, 300);
+        image(flower, x + 50, 400);
+        image(flower, x, 500);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //logic + events
+    //character movement
+
+    var shadowY = kazuhiroY + 55
+    var kazuhiroIsWalking = false;
+    var kazuhiroIsLeft = true;
+    var W = 87;
+    var A = 65;
+    var S = 83;
+    var D = 68;
+    
+    var UP = 38;
+    var RIGHT = 39;
+    var DOWN = 40;
+    var LEFT = 37;
+
+    
+    
+
+    if (keyIsDown(W) || keyIsDown(UP)) {
+        //background(200); //putting the bg here redraws it every frame so the frames of the transparent gif arent visible
+        kazuhiroY -= kazuhiroSpeed;
+        kazuhiroIsWalking = true;
+        kazuhiroIsLeft = true;
+
+    }
+
+    if (keyIsDown(S) || keyIsDown(DOWN)) {
+        //background(200); //putting the bg here redraws it every frame so the frames of the transparent gif arent visible
+        kazuhiroY += kazuhiroSpeed;
+        kazuhiroIsWalking = true;
+        kazuhiroIsLeft = false;
+    }
+
+    if (keyIsDown(A) || keyIsDown(LEFT)) {
+        //background(200); //putting the bg here redraws it every frame so the frames of the transparent gif arent visible
+        kazuhiroX -= kazuhiroSpeed;
+        kazuhiroIsWalking = true;
+        kazuhiroIsLeft = true;
+    }
+
+    if (keyIsDown(D) || keyIsDown(RIGHT)) {
+        //background(200); //putting the bg here redraws it every frame so the frames of the transparent gif arent visible
+        kazuhiroX += kazuhiroSpeed;
+        kazuhiroIsWalking = true;
+        kazuhiroIsLeft = false;
+    }
+
+
+
+    if (kazuhiroIsLeft === true && kazuhiroIsWalking === true) {
+        //background(200);
+        image(shadow, kazuhiroX, shadowY);
+        image(kazuhiroWalkL, kazuhiroX, kazuhiroY);
+        
+    }
+    if (kazuhiroIsLeft === false && kazuhiroIsWalking === true) {
+        //background(200);
+        image(shadow, kazuhiroX, shadowY);
+        image(kazuhiroWalkR, kazuhiroX, kazuhiroY);
+        
+    }
+    if (kazuhiroIsWalking === false) {
+        //background(200);
+        image(shadow, kazuhiroX, shadowY);
+        image(kazuhiroIdleL, kazuhiroX, kazuhiroY);
+        
+    }
+
+    /* added the background to each wasd if so that if multiple keys were pressed at once you cant see both animations. It was the only thing i could think of lmao but it worked*/
+
+
+    //OUT OF BOUNDS
+    //left
+    if (kazuhiroX <= 23) {
+        if (keyIsDown(A) || keyIsDown(LEFT)) {
+            kazuhiroX = 23;
+        }
+    }
+    //right
+    if (kazuhiroX >= 910) {
+        if (keyIsDown(D) || keyIsDown(RIGHT)) {
+            kazuhiroX = 910;
+        }
+    }
+    //up
+    if (kazuhiroY <= 63) {
+        if (keyIsDown(W) || keyIsDown(UP)) {
+            kazuhiroY = 63;
+        }
+    }
+    //down
+    if (kazuhiroY >= 490) {
+        if (keyIsDown(S) || keyIsDown(DOWN)) {
+            kazuhiroY = 490;
+        }
+    }
+    
+
+    
+
+
+    //reference text;
+    var canvasWidth = width;
+    var canvasHeight = height;
+
+    stroke('white');
+    strokeWeight(3);
+    textSize(15);
+
+    text('mouseX:' + round(mouseX), canvasWidth - 200, 15);
+    text('mouseY:' + round(mouseY), canvasWidth - 200, 30);
+    text('canvas size: ' + width + ', ' + height, canvasWidth - 200, 45);
+    text('keycodes: ' + `${key} ${keyCode}`, canvasWidth - 200, 60);
+    text('W:87', canvasWidth - 155, 80);
+    text('A:65', canvasWidth - 185, 100);
+    text('S:83', canvasWidth - 155, 120);
+    text('D:68', canvasWidth - 120, 100);
+
+
+    text('kazuhiro position: ' + kazuhiroX + ', ' + kazuhiroY, 10, 20);
+    text('is kazuhiro walking? ' + kazuhiroIsWalking, 10, 40);
+    text('clouds: ' + cloudCounter, 10, 60);
+
+
+
+
+}
